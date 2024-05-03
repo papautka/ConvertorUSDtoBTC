a16713b4edea3de4b965dd8864260b7226c3b7b1
andrew <120500uas@gmail.com>
2024-05-03 20:45:14 +0300

    get_price_list_from_raw_data

--- app/src/main/java/com/uteev/convertorusdtobtc/presentation/CoinViewModel.kt
+++ app/src/main/java/com/uteev/convertorusdtobtc/presentation/CoinViewModel.kt
@@ -3,8 +3,11 @@ package com.uteev.convertorusdtobtc.presentation
 import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import com.google.gson.Gson
 import com.uteev.convertorusdtobtc.data.apiservice.ApiFactory
 import com.uteev.convertorusdtobtc.data.database.AppDatabase
+import com.uteev.convertorusdtobtc.domain.pojo.coinprice.CoinPriceInfo
+import com.uteev.convertorusdtobtc.domain.pojo.coinprice.CoinPriceInfoRawData
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
@@ -17,17 +20,50 @@ class CoinViewModel(application: Application) : AndroidViewModel(application) {
 
     fun loadData() {
         val disposbale = ApiFactory.apiService.getTopCoinsInfo()
+            .map { it.data?.map { it.coinInfo?.name }?.joinToString(",") }
+            .flatMap { ApiFactory.apiService.getFullPriceList(fromSymbol = it) }
+            .map { getPriceListFromRawData(it) }
             .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                val namesCoinInfo = it.data?.map { it.coinInfo?.name }?.joinToString(",")
-                Log.d("namesCoinInfo", namesCoinInfo.toString())
                 Log.d("SUCCESS_OF_LOADING_DATA", it.toString())
+                Thread {
+                    db.coinPriceInfoDao().insertPriceList(it)
+                }.start()
             },{
                 Log.d("FAIL_OF_LOADING_DATA", it.message.toString())
             })
         compositeDisposable.add(disposbale)
     }
+
+    private fun getPriceListFromRawData(
+        coinPriceInfoRawData: CoinPriceInfoRawData
+    ) : List<CoinPriceInfo> {
+        val result = ArrayList<CoinPriceInfo>()
+
+        // Получаем JsonObject из CoinPriceInfoRawData
+        val jsonObject = coinPriceInfoRawData.coinPriceInfoJsonObject
+        if (jsonObject == null) {
+            return result
+        }
+        // Получаем KeySet из JsonObject (Каждая пара криптовалюты имеет свой уникальный Key)
+        val coinKeySet = jsonObject.keySet()
+
+        // Перебираем все пары криптовалют и получаем CoinPriceInfo
+        for(key in coinKeySet) {
+            val currencyJson = jsonObject.getAsJsonObject(key)
+            val currencyKeySet = currencyJson.keySet()
+
+            for (currencyKey in currencyKeySet) {
+                val priceInfo = Gson().fromJson(
+                    currencyJson.getAsJsonObject(currencyKey),
+                    CoinPriceInfo::class.java
+                )
+                result.add(priceInfo)
+            }
+        }
+        return result
+    }
+
     override fun onCleared() {
         super.onCleared()
         compositeDisposable.dispose()
